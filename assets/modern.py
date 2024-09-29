64fcaa81c43ba1e0b257676ba743426a20aa7a8b
andre <andreamusholin@gmail.com>
2024-09-29 20:33:40 +0700

    first commit

---
+++ assets/modern.py
@@ -0,0 +1,7 @@
+
+from pathlib import Path
+
+# from tkinter import *
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, ttk
+import tkinter as tk
